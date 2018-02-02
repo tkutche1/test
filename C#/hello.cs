d9137dfcf8803a1857fcb21be2bc63b5a6118768
Tim Kutcher <tkutche1@jhu.edu>
2018-02-02 13:24:17 -0500

    C# and Shell

---
+++ C#/hello.cs
@@ -0,0 +1,13 @@
+// hello.cs - Hello  world in C#
+
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
