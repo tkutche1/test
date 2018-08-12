055eac31aa5ad36f6ab15923d068cb03859f9cc1
Tim Kutcher <tkutcher@outlook.com>
2018-08-12 15:44:40 -0400

    JavaScript

---
+++ JavaScript/hello.js
@@ -0,0 +1,8 @@
+// Tim Kutcher
+// hello.js - Hello world in JavaScript.
+
+function hello(name) {
+    console.log('Hello, World! My name is ' + name + '.')
+}
+
+hello('JavaScript')
---
+++ JavaScript/run.sh
@@ -0,0 +1,6 @@
+# !/usr/bin/sh
+
+# Tim Kutcher
+# JavaScript/run.sh
+
+node hello.js
